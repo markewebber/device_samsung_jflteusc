e720b97ff101c76bbdc1c4cb7d9df1c4a13fc988
blk_jack <jcorcoran+github@gmail.com>
2013-05-10 13:08:58 -0400

    Initial commit

---
+++ slim.mk
@@ -0,0 +1,32 @@
+$(call inherit-product, device/samsung/jfltetmo/full_jfltetmo.mk)
+
+# Release name
+PRODUCT_RELEASE_NAME := jfltetmo
+
+# Inherit some common SLIM stuff.
+$(call inherit-product, vendor/slim/config/gsm.mk)
+
+# Enhanced NFC
+$(call inherit-product, vendor/slim/config/nfc_enhanced.mk)
+
+# Inherit some common SLIM stuff.
+$(call inherit-product, vendor/slim/config/common_full_phone.mk)
+
+
+# Inherit device settings
+$(call inherit-product, vendor/slim/config/common_sgs.mk)
+
+# Inherit torch settings
+$(call inherit-product, vendor/slim/config/common_ledflash.mk)
+
+PRODUCT_COPY_FILES +=  \
+    vendor/slim/prebuilt/hdpi/bootanimation.zip:system/media/bootanimation.zip
+
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=jfltetmo TARGET_DEVICE=jfltetmo BUILD_FINGERPRINT="samsung/jfltetmo/jfltetmo:4.2.2/JDQ39/M919UVUAMDB:user/release-keys" PRIVATE_BUILD_DESC="jfltetmo-user 4.2.2 JDQ39 M919UVUAMDB release-keys"
+
+PRODUCT_NAME := slim_jfltetmo
+PRODUCT_DEVICE := jfltetmo
+PRODUCT_BRAND := samsung
+PRODUCT_MODEL := SGH-M919
+PRODUCT_MANUFACTURER := Samsung
+
