f0299c3cdfb6b1112743042dc00cf64defa08a35
nicklovell23 <nicklovell23@gmail.com>
2014-11-26 06:40:22 +0100

    Slim

--- slim.mk
+++ slim.mk
@@ -6,10 +6,15 @@ $(call inherit-product, vendor/slim/config/nfc_enhanced.mk)
 # Inherit some common SLIM stuff.
 $(call inherit-product, vendor/slim/config/common_full_phone.mk)
 
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=jflteusc TARGET_DEVICE=jflteusc BUILD_FINGERPRINT="samsung/jflteusc/jflteusc:4.2.2/JDQ39/R970VXUAMDB:user/release-keys" PRIVATE_BUILD_DESC="jflteusc-user 4.2.2 JDQ39 R970VXUAMDB release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += 
+    PRODUCT_NAME=jflteusc 
+    TARGET_DEVICE=jflteusc 
+    BUILD_FINGERPRINT="samsung/jflteusc/jflteusc:4.2.2/JDQ39/R970VXUAMDB:user/release-keys" 
+    PRIVATE_BUILD_DESC="jflteusc-user 4.2.2 JDQ39 R970VXUAMDB release-keys"
+
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_NAME := slim_jflteusc
-PRODUCT_DEVICE := jflteusc
+
 
 
