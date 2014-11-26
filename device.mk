79b9883bc4fbfa7186f3bb1cbd0057a3fd9501f6
nicklovell23 <nicklovell23@gmail.com>
2014-11-25 21:38:30 -0600

    Update device.mk

--- device.mk
+++ device.mk
@@ -2,11 +2,11 @@
 ## Also get non-open-source specific aspects if available
 $(call inherit-product-if-exists, vendor/samsung/jflter970/jflter970-vendor.mk)
 
+# Disable MSB for GPS
+NEEDS_GPS_MSB_DISABLED := true
+
 ## overlays
 DEVICE_PACKAGE_OVERLAYS += device/samsung/jflteusc/overlay
-
-## common overlays
-DEVICE_PACKAGE_OVERLAYS += device/samsung/jf-common/overlay-cdma
 
 # Inherit from jf-common
 $(call inherit-product, device/samsung/jf-common/jf-common.mk)
